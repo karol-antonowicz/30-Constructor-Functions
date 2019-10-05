9bae10ea2d9aedd9b5233d9c605b6f4b67727df5
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-05 23:54:45 +0200

    initial commit: added test.js wiith some simple getelementsByTagName for example

---
+++ test.js
@@ -0,0 +1,9 @@
+// document.getElementsByClassName("one")); //
+
+var myOneDivs = document.getElementsByClassName("one");
+
+// myOneDivs[0].getElementsByTagName("p"); //
+
+var myP = myOneDivs[0].getElementsByTagName("p");
+
+myP[0].innerHTML = "Siemasz Karol";
